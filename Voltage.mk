6c85e86ef6da44b1171b163a04c06b169fc45aff
ahnet-69 <syedmusicrealise@gmail.com>
2023-10-16 05:50:21 +0500

    Move read only flag to Voltage.mk (this patch keeps failing i am done with it)

--- Voltage.mk
+++ Voltage.mk
@@ -11,6 +11,7 @@ TARGET_BOOT_ANIMATION_RES := 1920
 
 TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
 TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
+BOARD_EXT4_SHARE_DUP_BLOCKS := true
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.system.ota.json_url=https://raw.githubusercontent.com/ahnet-69/treble_voltage/14.0/ota.json
